dee43184fd9f778350b2b0d69eaa28bea5bd6747
emm <723125@gmail.com>
2021-01-08 11:53:30 +0000

    Initial Commit

---
+++ 08_animatedSplashScreen/_8_animatedSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_animatedSplashScreenApp.swift
+//  08_animatedSplashScreen
+//
+//  Created by emm on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct _8_animatedSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
